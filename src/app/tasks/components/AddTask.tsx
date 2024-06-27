7506f7ff1a489bba3b30a9f11b4166cbdca34797
Arzoyan <ruben.arzoyan@gmail.com>
2024-06-28 03:35:17 +0400

    add new to do button styles changed

--- src/app/tasks/components/AddTask.tsx
+++ src/app/tasks/components/AddTask.tsx
@@ -27,7 +27,7 @@ const AddTask: React.FC<InputProps> = ({ value, onChange, onPressEnter, placehol
       />
       <button
         onClick={onPressEnter}
-        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+        className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded mr-2.5"
         type="button"
       >
         Add
